c6126a25849ab1792abc22e591caddc1ae390ef8
Sajad Hashemian <sajadhsm@gmail.com>
2019-01-20 17:20:29 +0330

    improve index and add npm start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const antlr4 = require('antlr4')
 const liluLexer = require('./lib/lilu_grammarLexer')
 const liluParser = require('./lib/lilu_grammarParser')
+const Listener = require('./Listener')
 const fs = require('fs')
 const input = fs.readFileSync('./.test',{encoding: 'utf8'})
 
@@ -8,6 +9,11 @@ const chars = new antlr4.InputStream(input)
 const lexer = new liluLexer.lilu_grammarLexer(chars)
 const tokens = new antlr4.CommonTokenStream(lexer)
 const parser = new liluParser.lilu_grammarParser(tokens)
-const tree = parser.program()
 
-console.log(tree.toStringTree(parser.ruleNames))
+try {
+  const tree = parser.program();
+  let listener = new Listener();
+  antlr4.tree.ParseTreeWalker.DEFAULT.walk(Listener, tree);
+} catch (error) {
+  console.log(error);
+}
